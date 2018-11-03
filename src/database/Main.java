8bc5ae234c39b2f9d8df921211ddd2aa73ef94a0
Sakib1127 <30300839+Sakib1127@users.noreply.github.com>
2018-11-04 02:59:58 +0600

    Add files via upload

--- src/database/Main.java
+++ src/database/Main.java
@@ -8,11 +8,10 @@ public class Main {
        // Show_question_with_options show = new Show_question_with_options(subject);
        //System.out.println(show.question[1]);
         int id = 1;
-       Add_new_subject new_subject = new Add_new_subject("data_structure");
-
-        for(int i = 0; i<4; i++) {
-            Insert_question insert_question = new Insert_question("data_structure");
-        }
+       //Add_new_subject new_subject = new Add_new_subject("data_structure");
+        //for(int i = 0; i<4; i++) {
+           // Insert_question insert_question = new Insert_question("data_structure");
+        //}
         //Delete_question_answer delete_question_answer = new Delete_question_answer(subject,id);
     }
 }
